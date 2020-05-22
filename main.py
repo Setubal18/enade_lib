653307f389a53923b0a3e91c93163d9faa82e2b9
Leonardo Setubal <setubal43@gmail.com>
2020-05-22 15:19:26 -0300

    feat: no main adicionado path temporário para execução da lib

---
+++ main.py
@@ -0,0 +1,23 @@
+from utils.change_vars import lowerVars, updatedVars
+from utils.covert_to_dict import transformDict
+from utils.read_archive import readArchive
+from utils.new_atributes import new_atributes
+from course_patterns.course import patternShifts
+
+path = 'Dados/2017exp.txt'
+
+
+def execute():
+	keys, data = readArchive(path)
+	keys = lowerVars(keys)
+	keys = updatedVars(keys)
+	enadeData = transformDict(keys, data)
+	print(enadeData)
+
+	for dict in enadeData:
+		dict = new_atributes(dict)
+		dict = patternShifts(dict)
+		print(dict)
+
+
+execute()
