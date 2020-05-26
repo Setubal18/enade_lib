1023baa57c30fc17013a48c43986063744aced5a
Leonardo Setubal <setubal43@gmail.com>
2020-05-26 17:11:41 -0300

    refact: mudança dos valores dos turnos

--- course_patterns/shifts.py
+++ course_patterns/shifts.py
@@ -1,28 +1,28 @@
 def shiftsPatterns2004_2010(dict):
 	try:
 		if int(dict['in_matut']) == 1:
-			dict["varCursoTurnos"].update({'varCursoMatutino': True})
+			dict["varCursoTurnos"].update({'varCursoMatutino': 1})
 			del dict['in_matut']
 		else:
-			dict["varCursoTurnos"].update({'varCursoMatutino': False})
+			dict["varCursoTurnos"].update({'varCursoMatutino': 0})
 			del dict['in_matut']
 
 		if int(dict['in_vesper']) == 1:
-			dict["varCursoTurnos"].update({'varCursoVespertino': True})
+			dict["varCursoTurnos"].update({'varCursoVespertino': 2})
 			del dict['in_vesper']
 		else:
-			dict["varCursoTurnos"].update({'varCursoVespertino': False})
+			dict["varCursoTurnos"].update({'varCursoVespertino': 0})
 			del dict['in_vesper']
 
 		if int(dict['in_noturno']) == 1:
-			dict["varCursoTurnos"].update({'varCursoNoturno': True})
+			dict["varCursoTurnos"].update({'varCursoNoturno': 4})
 			del dict['in_noturno']
 		else:
-			dict["varCursoTurnos"].update({'varCursoNoturno': False})
+			dict["varCursoTurnos"].update({'varCursoNoturno': 0})
 			del dict['in_noturno']
 
 	except ValueError:
-		dict["varCursoTurnos"].update({'faltandoTurno': True})
+		dict["varCursoTurnos"].update({'faltandoTurno': 0})
 		del dict['in_noturno']
 		del dict['in_vesper']
 		del dict['in_matut']
@@ -33,17 +33,17 @@ def shiftsPatterns2004_2010(dict):
 def shiftsPatterns2015_2018(dict):
 	try:
 		if int(dict['co_turno_graduacao']) == 1:
-			dict["varCursoTurnos"].update({'varCursoMatutino': True})
+			dict["varCursoTurnos"].update({'varCursoMatutino': 1})
 			del dict['co_turno_graduacao']
 		elif int(dict['co_turno_graduacao']) == 2:
-			dict["varCursoTurnos"].update({'varCursoVespertino': True})
+			dict["varCursoTurnos"].update({'varCursoVespertino': 2})
 			del dict['co_turno_graduacao']
 		elif int(dict['co_turno_graduacao']) == 3:
-			dict["varCursoTurnos"].update({'varCursoIntegral': True})
+			dict["varCursoTurnos"].update({'varCursoIntegral': 3})
 			del dict['co_turno_graduacao']
 		elif int(dict['co_turno_graduacao']) == 4:
-			dict["varCursoTurnos"].update({'varCursoNoturno': True})
+			dict["varCursoTurnos"].update({'varCursoNoturno': 4})
 			del dict['co_turno_graduacao']
 	except ValueError:
-		dict["varCursoTurnos"].update({'faltandoTurno': True})
+		dict["varCursoTurnos"].update({'faltandoTurno': 0})
 	return dict
