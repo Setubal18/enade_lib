47495612493325a7cc2d34cafacc5d4fb3ddb71c
Leonardo Setubal <setubal43@gmail.com>
2020-05-26 14:43:28 -0300

    feat: Fortamando questões da percepção do usuário a respeito da prova

---
+++ Perception_of_proof/add_atributes.py
@@ -0,0 +1,31 @@
+from utils.globals import re
+
+def version_2004_2010(versao_2004_2010, i, dict):
+	dict['percepcao_prova'].update(
+		{' co_rs_i' + str(i): dict[versao_2004_2010]}
+	)
+	return dict
+
+
+def version_2011Plus(versao_2011plus, dict):
+	dict['percepcao_prova'].update(
+		{versao_2011plus: dict[versao_2011plus]}
+	)
+	return dict
+
+
+def quantity_questions(dict):
+	for keys in list(dict.keys()):
+		nu_que_qip = re.search('nu_que_qip', keys)
+		if nu_que_qip:
+			dict['percepcao_prova'].update(
+				{nu_que_qip.string: dict[nu_que_qip.string]}
+			)
+		else:
+			leng = len(dict['percepcao_prova'])
+			dict['percepcao_prova'].update(
+				{
+					'nu_que_qip': str(leng)
+				}
+			)
+		return dict
--- utils/new_atributes.py
+++ utils/new_atributes.py
@@ -1,5 +1,5 @@
 def new_atributes(dict):
 	dict["varCursoTurnos"] = {}
 	dict['socioEconomico_Questionario'] = {}
-
+	dict['percepcao_prova'] = {}
 	return dict
