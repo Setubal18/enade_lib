30c71d59354c5595962165bbfdfa38c0702dbe17
Leonardo Setubal <setubal43@gmail.com>
2020-05-26 16:53:42 -0300

    feat: adicionado notas

--- main.py
+++ main.py
@@ -4,22 +4,25 @@ from utils.read_archive import readArchive
 from utils.new_atributes import new_atributes
 from course_patterns.course import patternShifts
 from economic_questions.economic_questions import formatQuestions_EconomicPartner
-from Perception_of_proof.format_Proof_Perception import formatQuestions_proofPerception
-path = 'Dados/2017exp.txt'
-
+from perception_of_proof.format_Proof_Perception import formatQuestions_proofPerception
+from grades.format_grades import format_grades
+path = 'Dados/2018exp.txt'
+#2004exp.csv
+#2013exp.txt
+#2018exp.txt
+#2011exp.txt
 
 def execute():
 	keys, data = readArchive(path)
 	keys = lowerVars(keys)
 	keys = updatedVars(keys)
 	enadeData = transformDict(keys, data)
-	print(enadeData)
 
 	for dict in enadeData:
-		dict = new_atributes(dict)
-		dict = patternShifts(dict)
-		dict = formatQuestions_EconomicPartner(dict)
-		dict = formatQuestions_proofPerception(dict)
+		patternShifts(dict)
+		formatQuestions_EconomicPartner(dict)
+		formatQuestions_proofPerception(dict)
+		format_grades(dict)
 		print(dict)
 
 
