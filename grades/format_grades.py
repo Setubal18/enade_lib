a5a2d851f9702af1261c544ba8fbbaa1536d6bcb
Leonardo Setubal <setubal43@gmail.com>
2020-05-26 16:49:58 -0300

    feat: formatação de notas

---
+++ grades/format_grades.py
@@ -0,0 +1,50 @@
+from utils.globals import re
+from utils.format_dict import format_dict
+
+
+def grades_ce_discursive_indiv(dict):
+	dict['notas_ce_d_indi'] = {}
+	for keys in list(dict.keys()):
+		nt_ce = re.search('nt_ce_d\d+', keys)
+		if nt_ce:
+			nt_ce = nt_ce.string
+			dict['notas_ce_d_indi'].update(
+				{keys: dict[nt_ce]}
+			)
+			del dict[keys]
+
+	return format_dict(dict, 'notas_ce_d_indi', 'notas')
+
+
+def grades_ce_geral(dict):
+	dict['notas_ce'] = {}
+	for keys in list(dict.keys()):
+		nt_ce = re.search('((nt_\w+ce)|(nt_ce_\w+))', keys)
+		if nt_ce:
+			nt_ce = nt_ce.string
+			dict['notas_ce'].update(
+				{keys: dict[nt_ce]}
+			)
+			del dict[keys]
+	return format_dict(dict, 'notas_ce', 'notas')
+
+
+def grades_fg(dict):
+	dict['notas_fgs'] = {}
+	for keys in list(dict.keys()):
+		nt_fg = re.search('((nt_\w+fg)|(nt_fg_\w+))', keys)
+		if nt_fg:
+			nt_fg = nt_fg.string
+			dict['notas_fgs'].update(
+				{keys: dict[nt_fg]}
+			)
+			del dict[keys]
+	return format_dict(dict, 'notas_fgs', 'notas')
+
+
+def format_grades(dict):
+	grades_ce_discursive_indiv(dict)
+	grades_ce_geral(dict)
+	grades_fg(dict)
+	format_dict(dict, 'nt_ger', 'notas')
+	return dict
