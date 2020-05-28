02ffba97f1e093f415861084abb6aa4611db7895
Leonardo Setubal <setubal43@gmail.com>
2020-05-28 18:55:06 -0300

    feat: Realizado modificações necessarias para salvar em um bd

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from utils.change_vars import lowerVars, updatedVars
-from utils.covert_to_dict import transformDict
+from database.save import create_many
 from utils.read_archive import readArchive
-from utils.generateCode import generateCode
-from utils.format_dict import format_dict
+from utils.format_dict import format_dict, format_array_dict
 from course_patterns.course import patternShifts
 from economic_questions.economic_questions import formatQuestions_EconomicPartner
 from perception_of_proof.format_Proof_Perception import formatQuestions_proofPerception
@@ -10,15 +8,13 @@ from grades.format_grades import format_grades
 from vectors.group_vectors import agroup_vectors
 from institution_student.agroup import institution, students
 import situations
+import database
 
 
 def execute():
-	path = input('caminho',)
+	path = input('caminho:', )
 	keys, data = readArchive(path)
-	keys = lowerVars(keys)
-	keys = updatedVars(keys)
-	enadeData = transformDict(keys, data)
-	enadeData = generateCode(enadeData)
+	enadeData = format_array_dict(keys, data)
 
 	for dict in enadeData:
 		format_dict(dict, 'tempo', 'nu_ano')
@@ -30,6 +26,12 @@ def execute():
 		agroup_vectors(dict)
 		institution(dict)
 		students(dict)
+	database.save(enadeData)
 
 
-execute()
+
+i = 'y'
+while i.lower() == 'y':
+	execute()
+	print('Deseja salvar outro arquivo?(y = sim n = não)')
+	i = input('(y/n) = ')
